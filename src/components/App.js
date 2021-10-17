ebcc6013510a3dfc765e2007cafcac2343c40d21
young_hwan <jouny007@gm.gist.ac.kr>
2021-10-17 13:53:33 +0900

    using CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from "react";
 import AppRouter from "components/Router";
 import {authService} from "myBase";
-import {onAuthStateChanged} from "firebase/auth";
+import {onAuthStateChanged,updateProfile} from "firebase/auth";
 
 
 function App() {
@@ -10,14 +10,34 @@ function App() {
   useEffect(()=>{
     onAuthStateChanged(authService,(user)=>{
       if(user){
-        setUserObj(user);
+        if(user.displayName==null){
+          const ind = user.email.indexOf("@")
+          const end = user.email.substring(0,ind)
+          updateProfile(user,{displayName:end});
+        }
+        setUserObj({
+          displayName : user.displayName,
+          uid : user.uid,
+        });
+      }else{
+        setUserObj(null);
       }
       setInit(true)
     });
   },[]);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName : user.displayName,
+      uid : user.uid,
+    });
+  }
   return(
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing"}
+      {init ? <AppRouter 
+                refreshUser = {refreshUser}
+                isLoggedIn={Boolean(userObj)}
+                userObj={userObj}/> : "Initializing"}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,11 @@
-import {dbService, storgeService}  from "myBase";
-import { addDoc, collection,onSnapshot } from "firebase/firestore";
+import {dbService}  from "myBase";
+import { collection,onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import Nweet from "components/Nweet";
-import {ref, uploadString,getDownloadURL} from "firebase/storage";
-import { v4 as uuidv4 } from "uuid";
+import NweetFactory from "components/NweetFactory";
 
 const Home = ({userObj}) =>{
-
-    const [nweet,setNweet] = useState("");
     const [nweets,setNweets]= useState([]);
-    const [attachment,setAttachment] = useState("");
     useEffect(()=> {
         onSnapshot(collection(dbService,"nweets"),(snapshot)=>{
             const nweetArray = snapshot.docs.map((doc) => ({
@@ -20,61 +16,11 @@ const Home = ({userObj}) =>{
             setNweets(nweetArray)
         });
     },[]);
-    const onSubmit = async (event) =>{
-        event.preventDefault();
-        let attachmentUrl="";
-        if(attachment!==""){
-            const attachmentRef = ref(storgeService, userObj.uid+'/'+uuidv4());
-            const response = await uploadString(attachmentRef,attachment,"data_url");
-            attachmentUrl = await getDownloadURL(response.ref);
-        }
-        const nweetObj={
-            text : nweet ,
-            createdAt: Date.now(),
-            creatorId : userObj.uid,
-            attachmentUrl,
-        }
-        await addDoc(collection(dbService,"nweets"),nweetObj);
-        setNweet("")
-        setAttachment("")
-    };
-    const onChange = (event) =>{
-        const {target:{value}} = event;
-        setNweet(value)
-    };
-    const onFileChange = (event) => {
-        const {target:{files}}=event;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) =>{
-            const{
-                currentTarget :{result},
-            } = finishedEvent;
-            setAttachment(result)
-        }
-        reader.readAsDataURL(theFile);
 
-    }
-    const onClearAttachment = () => setAttachment("");
     return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <input 
-                    value={nweet}
-                    onChange={onChange}
-                    type="text" 
-                    placeholder="what's on your mind?" 
-                    maxLength={120}/>
-                <input type="file" accept="image/*" onChange={onFileChange} alt="profile"/>
-                <input type="submit" value="Nweet"/>
-                {attachment &&(
-                    <div>
-                        <img src={attachment} width="50px" height="50px"/>
-                        <button onClick={onClearAttachment}>Clear</button>
-                    </div>
-                )}
-            </form>
-            <div>
+        <div className="container">
+            <NweetFactory userObj={userObj}/>
+            <div style={{marginTop:30}}>
                 {nweets.map((nweet)=>(
                     <Nweet
                         key={nweet.id}
