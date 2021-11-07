16ef2192564b153f52056f95d97631a8445e81cb
Chaeyul-K <91933277+Chaeyul-K@users.noreply.github.com>
2021-11-08 00:20:18 +0900

    Add files via upload

---
+++ src/components/Router.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+
+const AppRouter = ({ isLoggedIn, userObj }) => {
+    return (
+      <Router>
+        {isLoggedIn && <Navigation />}
+        <Switch>
+            {isLoggedIn ? (
+            <>
+              <Route exact path="/">
+                  <Home userObj={userObj} />
+              </Route>
+              <Route exact path="Profile">
+               <Profile />
+              </Route>
+            </>
+            ) : (
+              <>
+              <Route exact path="/Auth">
+              <Auth />
+          </Route>)
+          </>
+            )}
+        </Switch>
+      </Router>
+    );
+};
+
+export default AppRouter;
